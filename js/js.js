38f83e6c80beec3b5d80f40854940c76634f9a9c
phertyameen <aminubabafatima8@gmail.com>
2024-07-19 13:02:39 +0100

    restructured js

---
+++ js/js.js
@@ -0,0 +1,62 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     // Select the popup div and buttons
+//     const popUpSect = document.getElementById('popUpSect');
+//     const cancelBtn = document.querySelector('.cancelTRMBtn');
+//     const submitBtn = document.querySelector('.submitTRMBtn');
+//     const generateBtn = document.querySelector('.generateBtn');
+
+//     const hidden = document.querySelector('.hidden');
+
+//     const successPopUpSect = document.getElementById('successPopUpSect');
+//     const exitBtn = document.getElementById('exit');
+
+//     // Add event listener to the generate button
+//     generateBtn.addEventListener('click', () => {
+//         // Show the popup
+//         popUpSect.style.display = 'block';
+//         hidden.style.display = 'block'
+//     });
+
+//     // Add event listener to the cancel button
+//     cancelBtn.addEventListener('click', () => {
+//         // Hide the popup
+//         popUpSect.style.display = 'none';
+//     });
+
+//     // Add event listener to the submit button
+//     submitBtn.addEventListener('click', () => {
+//         // Hide the popup
+//         popUpSect.style.display = 'none';
+
+//         // Show the success popup
+//         successPopUpSect.style.display = 'block';
+//     });
+
+//     exitBtn.addEventListener('click', (event) => {
+//         // Prevent the default action of the anchor tag
+//         event.preventDefault();
+        
+//         // Hide the success popup
+//         successPopUpSect.style.display = 'none';
+//     });
+
+
+
+//     // add event listeners for buttons in the success message popup respectively
+
+//     document.querySelector('#goToRequestBtn').addEventListener('click', () => {
+//         //navigate to the request list
+//         window.location.href = './requests.html';
+//     });
+
+//     document.querySelector('#goToCreateInspection').addEventListener('click', () => {
+//         //navigate to the new inspection pade
+//         window.location.href = './newInspection.html';
+//     });
+
+//         // Add an event listener to the label to trigger the file input click
+//     document.querySelector('label[for="fileInput"]').addEventListener('click', function() {
+//         document.getElementById('fileInput').click();
+//     });
+
+// });
---
+++ js/jsAll.js
@@ -0,0 +1,106 @@
+// dashboard script
+// changes section from overview to requests
+const requests = document.querySelector('.requests')
+
+requests.addEventListener('click', () => {
+
+    window.location.href = 'requests.html'
+    requests.style.color = '#ff7a00'
+})
+
+// redirects to new inspection page on the click of a button
+const inspectPgBtn = document.querySelector('#inspectPgBtn')
+console.log(inspectPgBtn)
+
+inspectPgBtn.addEventListener('click', e => {
+    window.location.href = './newInspection.html';
+})
+
+// redirects back to overview section | initial dashboard
+const overview = document.querySelector('.overview')
+console.log(overview)
+
+overview.addEventListener('click', () => {
+    window.location.href = 'dashboard.html'
+})
+
+// getting a menu icon to dispay a dropdown in a mobile view
+
+let menuIcon = document.querySelector('#menuIcon')
+let exitIcon = document.querySelector('.exitIcon')
+console.log(menuIcon, exitIcon)
+
+menuIcon.addEventListener('click', () => {
+    const sideNavSect = document.querySelector('.sideNavSect')
+    // sideNavSect.style.display = 'flex'
+    sideNavSect.classList.toggle('show');
+    // exitIcon.style.display = 'block' 
+})
+
+exitIcon.addEventListener('click', () => {
+    console.log(exitIcon)
+    const sideNavSect = document.querySelector('.sideNavSect')
+    // sideNavSect.style.display = 'flex'
+    sideNavSect.classList.remove('show');
+})
+
+// redirects to ongoingPg section
+const ongoingPg = document.querySelector('#ongoingPg')
+
+ongoingPg.addEventListener('click', () => {
+    window.location.href = 'rovenProp.html'
+})
+
+// rotating between all requests, ongoing and completed sections
+
+const allRequests = document.querySelector('#allRequestSpan')
+const ongoing = document.querySelector('#ongoingRequestSpan')
+const completed = document.querySelector('#completeRequestSpan')
+
+const statusOngoing = document.querySelector('.ongoingDuplicate')
+const statusOngoing1 = document.querySelector('.statusOngoingTd')
+
+const statusComplete = document.querySelector('#completeDuplicate')
+const statusComplete1 = document.querySelector('#statusCompleteTd')
+
+allRequests.addEventListener('click', () =>{
+    allRequests.style.backgroundColor = '#ffffff'
+    ongoing.style.backgroundColor = '#eee'
+    completed.style.backgroundColor = '#eee'
+    
+    statusOngoing1.style.display = 'inline'
+    statusComplete.style.display = 'none'
+
+    statusComplete1.style.display = 'inline'
+    statusOngoing.style.display = 'none'
+})
+
+ongoing.addEventListener('click', () =>{
+    ongoing.style.backgroundColor = '#ffffff'
+    allRequests.style.backgroundColor = '#eee'
+    completed.style.backgroundColor = '#eee'
+
+    statusOngoing1.style.display = 'inline'
+    statusComplete.style.display = 'none'
+
+    statusOngoing.style.display = 'inline'
+    statusComplete1.style.display = 'none'
+
+    // hide inspectPgBtn
+    inspectPgBtn.style.display = 'none'
+})
+
+completed.addEventListener('click', () =>{
+    completed.style.backgroundColor = '#ffffff'
+    ongoing.style.backgroundColor = '#eee'
+    allRequests.style.backgroundColor = '#eee'
+
+    statusOngoing1.style.display = 'none'
+    statusComplete.style.display = 'inline'
+
+    statusOngoing.style.display = 'none'
+    statusComplete1.style.display = 'block'
+
+    // hide inspectPgBtn
+    inspectPgBtn.style.display = 'none'
+})
